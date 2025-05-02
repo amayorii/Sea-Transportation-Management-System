739c332edddf86249b9d59b39f474209a81d7486
Nazariy <malashenko.naz@gmail.com>
2025-05-02 19:15:50 +0300

    quick tests

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using Sea_Transportation_Management_System.Model.Transportable;
+using Sea_Transportation_Management_System.Model.Vessels;
 
 namespace Sea_Transportation_Management_System;
 
@@ -10,5 +12,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Container container = new Container("containerWithSand", 50);
+        container.Add("nigger");
+        container.Add("sausage");
+        container.Add("box");
+        container.Add("sofa");
+        ContainerShip c1 = new ContainerShip(1, "Sea Queen", 250, 60);
+        c1.LoadCargo(container);
+        c1.ViewCargo();
+        // do table and automatic id setting
+        // make more transportable things (fluids, passengers)
     }
 }
